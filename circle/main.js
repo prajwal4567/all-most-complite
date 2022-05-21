e9ca7a5f816c4fca503254f91c52283b9f4df803
prajwal4567 <82152156+prajwal4567@users.noreply.github.com>
2022-05-21 17:30:11 +0530

    Add files via upload

--- circle/main.js
+++ circle/main.js
@@ -4,36 +4,40 @@ const ctx = canvas.getContext('2d');
 canvas.width=innerWidth;
 canvas.height=innerHeight;
 
-let xxs=0;
-let xxe=canvas.width;
-let xys=canvas.height/2;
-let xye=canvas.height/2;
-let yxs=canvas.width/2;
-let yxe=canvas.width/2;
-let yys=0;
-let yye=canvas.height;
-let centerx=canvas.width/2;
-let centery=canvas.height/2;
+let mx=0
+let my=0
+let xxs=0
+let xxe=0
+let xys=0
+let xye=0
+let yxs=0
+let yxe=0
+let yys=0
+let yye=0
 
-addEventListener("mousedown",moveline=()=>{
-    let mx=event.clientX;
-    let my=event.clientY;    
-    xxs=xxs+(centerx-mx);    
-    xxe=xxe+(centerx-mx);    
-    xys=xys+(centerx-my);    
-    xye=xye+(centerx-my);    
-    yxs=yxs+(centerx-mx);    
-    yxe=yxe+(centerx-mx);    
-    yys=yys+(centerx-my);    
-    yye=yye+(centerx-my); 
-    a=[xxs,xxe,xys,xye,yxs,yxe,yys,yye];
-    console.log(a);
-    ctx.beginPath();
-    ctx.moveTo(xxs,xys);
-    ctx.lineTo(xxe,xye);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(yxs,yys);
-    ctx.lineTo(yxe,yye);
-    ctx.stroke();
-});
+    function line(sx,sy,ex,ey){
+        ctx.beginPath();
+        ctx.moveTo(sx,sy);
+        ctx.lineTo(ex,ey);
+        ctx.stroke();
+    }
+    
+    ctx.line(0, 0, canvas.width, canvas.height);
+
+    addEventListener("mousedown",moveline=()=>{
+        mx=event.clientX;
+        my=event.clientY;
+        xxs=0;    
+        xxe=canvas.width;    
+        xys=my;    
+        xye=my;    
+        yxs=mx;   
+        yxe=mx;    
+        yys=0; 
+        yye=canvas.height;  
+        let x=line(xxs,xys,xxe,xye);
+        let y=line(yxs,yys,yxe,yye);                  
+        let xa=[x];
+        let ya=[y];
+        console.log(xa);
+    })
